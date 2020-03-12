c7f233b2f591cc1fac585c8ad36c954b704b2d26
denis_matafonov <dmatafonov2000@mail.ru>
2020-03-12 16:36:55 +0500

    Add method get_size_of_brain

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ public class Main {
         System.out.println("start");
         System.out.println("I'm not loh");
         int a = 0;
+        System.out.println(get_size_of_brain());
+    }
+
+    private static int get_size_of_brain(){
+        return 32567;
     }
 }
